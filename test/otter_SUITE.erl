1347c443c50ed28e44eabf7e7663dee509cca3c3
Heinz N. Gies <heinz@licenser.net>
2017-04-07 00:37:19 +0200

    Remove server dependency from tests

--- test/otter_SUITE.erl
+++ test/otter_SUITE.erl
@@ -1,5 +1,6 @@
 -module(otter_SUITE).
 -include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 -export([all/0, ptest/1, ftest/1, handle_span/1]).
 
@@ -7,37 +8,51 @@ all() ->
     [ftest, ptest].
 
 ptest(_Config) ->
-    application:ensure_all_started(otter),
-    application:set_env(otter, zipkin_collector_uri, "http://127.0.0.1:19411/api/v1/spans"),
-    application:set_env(otter, server_zipkin_callback, {?MODULE, handle_span}),
-    ets:new(test_span_collector, [named_table, public, {keypos, 2}]),
-    otter:span_pstart("test_span"),
-    otter:span_plog("started"),
-    otter:span_plog("α =:= ω"),
-    otter:span_ptag("result", "ok"),
-    otter:span_pend(),
+    application:ensure_all_started(otters),
+    application:set_env(otters, zipkin_collector_uri, "http://127.0.0.1:19411/api/v1/spans"),
+
+    meck:new(ibrowse, [passthrough]),
+    meck:expect(ibrowse, send_req, fun send_req/4),
+
+    otters:span_pstart("test_span"),
+    otters:span_plog("started"),
+    otters:span_plog("α =:= ω"),
+    otters:span_ptag("result", "ok"),
+    otters:span_pend(),
     timer:sleep(200),
-    [_] = ets:tab2list(test_span_collector).
+    ?assert(meck:validate(ibrowse)),
+    meck:unload(ibrowse).
 
 ftest(_Config) ->
-    application:ensure_all_started(otter),
-    application:set_env(otter, zipkin_collector_uri, "http://127.0.0.1:19411/api/v1/spans"),
-    application:set_env(otter, server_zipkin_callback, {?MODULE, handle_span}),
+    application:ensure_all_started(otters),
+    application:set_env(otters, zipkin_collector_uri, "http://127.0.0.1:19411/api/v1/spans"),
+    application:set_env(otters, server_zipkin_callback, {?MODULE, handle_span}),
     ets:new(test_span_collector, [named_table, public, {keypos, 2}]),
-    S1 = otter:span_start("test_span"),
-    S2 = otter:span_log(S1, "started"),
-    S3 = otter:span_tag(S2, "result", "ok"),
-    S4 = otter:span_log(S3, "α =:= ω"),
-    S5 = otter:span_log(S4, 123456),
-    S6 = otter:span_log(S5, 'this is a atom'),
-    S7 = otter:span_log(S6, io_lib:format("int: ~w, float: ~f, hex: ~.16B, Span: ~p",
-					  [1, 1.0, 1, S6])),
-    S8 = otter:span_log(S7, S7),
-    S9 = otter:span_log(S8, fun() -> "result of function" end),
-    otter:span_end(S9),
-    timer:sleep(200),
-    [_] = ets:tab2list(test_span_collector).
 
+    meck:new(ibrowse, [passthrough]),
+    meck:expect(ibrowse, send_req, fun send_req/4),
+
+    S1 = otters:span_start("test_span"),
+    S2 = otters:span_log(S1, "started"),
+    S3 = otters:span_tag(S2, "result", "ok"),
+    S4 = otters:span_log(S3, "α =:= ω"),
+    S5 = otters:span_log(S4, 123456),
+    S6 = otters:span_log(S5, 'this is a atom'),
+    S7 = otters:span_log(S6, io_lib:format("int: ~w, float: ~f, hex: ~.16B, Span: ~p",
+					  [1, 1.0, 1, S6])),
+    S8 = otters:span_log(S7, S7),
+    S9 = otters:span_log(S8, fun() -> "result of function" end),
+    otters:span_end(S9),
+    timer:sleep(200),
+    ?assert(meck:validate(ibrowse)),
+    meck:unload(ibrowse).
+
+
+send_req(_ZipkinURL,
+         [{"content-type", "application/x-thrift"}],
+         post, Data) ->
+    1 = length(otters_conn_zipkin:decode_spans(Data)),
+    {ok, {{stuff, 200, stuff}, stuff, stuff}}.
 
 handle_span(Span)  ->
     ets:insert(test_span_collector, Span).
